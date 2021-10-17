46507aa5d6ce150f884717a544156954d2620b7c
iamargus95 <kamathsuraj95@gmail.com>
2021-10-17 20:53:42 +0530

    Removed error fields from tests

--- Quiz_CLI/quiz_cli_test.go
+++ Quiz_CLI/quiz_cli_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"reflect"
 	"testing"
 	"time"
@@ -14,30 +13,23 @@ func TestParseCSV(t *testing.T) {
 	var testProblems = []struct {
 		file     string
 		expected []problem
-		err      error
 	}{
 		{
 			file:     "mock.csv",
 			expected: []problem{{"1+1", "2"}, {"2+2", "4"}, {"3+3", "6"}},
-			err:      nil,
 		},
 		{
 			file:     "mock1.csv",
 			expected: []problem{{"What is the capital of India?", "Delhi"}, {"When did India gain Independence?", "1947"}},
-			err:      nil,
 		},
 		{
 			file:     "nonexistent.csv",
 			expected: nil,
-			err:      errors.New("could not read file"),
 		},
 	}
 
 	for _, v := range testProblems {
-		got, err := parseCSV(v.file)
-		if !reflect.DeepEqual(err, v.err) {
-			t.Fail()
-		}
+		got, _ := parseCSV(v.file)
 		if !reflect.DeepEqual(got, v.expected) {
 			t.Fail()
 		}
@@ -46,16 +38,16 @@ func TestParseCSV(t *testing.T) {
 
 func testEachQuestion(t *testing.T) {
 	timer := time.NewTimer(time.Duration(2) * time.Second).C
-	done := make(chan string)
+	input := make(chan string)
 	answer := "2"
 	var ans int
 	var err error
 	allDone := make(chan bool)
 	go func() {
-		ans, err = eachQuestion(answer, timer, done)
+		ans, err = eachQuestion(answer, timer, input)
 		allDone <- true
 	}()
-	done <- "2"
+	input <- "2"
 
 	<-allDone
 	if err != nil {
