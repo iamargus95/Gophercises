cb2f285c67424edec56271c39bb358cbeb9b5241
iamargus95 <kamathsuraj95@gmail.com>
2021-10-17 16:00:50 +0530

    Working url shortner with flag for custom yaml file

--- Url_Shortner/url_shortner.go
+++ Url_Shortner/url_shortner.go
@@ -1,21 +1,60 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	yaml "gopkg.in/yaml.v3"
 )
+
+type pathURL struct {
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"`
+}
 
 func main() {
 
-	pathsToUrl := map[string]string{
-		"/github":   "https://github.com/iamargus95",
-		"/personal": "https://iamargus95.github.io/",
-		"/resume":   "https://iamargus95.github.io/assets/documents/Suraj_Kamath.pdf",
+	var yamlFile string
+	flag.StringVar(&yamlFile, "yaml", "default.yaml", "path to yaml file.")
+	flag.Parse()
+
+	yamlData, err := yamlReader(yamlFile)
+	if err != nil {
+		panic(err)
 	}
 
-	mapHandler := mapHandler(pathsToUrl, defaultMux())
+	yamlHandler, err := parseYAML([]byte(yamlData), defaultMux())
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("Server up and running at http://localhost:8080")
-	http.ListenAndServe(":8080", mapHandler)
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func yamlReader(filename string) ([]byte, error) {
+	yamlData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return yamlData, nil
+}
+
+func parseYAML(yamlData []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var paths []pathURL
+	err := yaml.Unmarshal(yamlData, &paths)
+	if err != nil {
+		return nil, err
+	}
+
+	pathsToUrls := make(map[string]string)
+	for _, v := range paths {
+		pathsToUrls[v.Path] = v.Url
+	}
+
+	return mapHandler(pathsToUrls, fallback), nil
 }
 
 func mapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
