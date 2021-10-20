54f454e4dc792d8b0d5572522f0c4a4cb5ced49c
iamargus95 <kamathsuraj95@gmail.com>
2021-10-20 13:21:41 +0530

    Added Structs to help decode JSON file

---
+++ Gophercises/Choose Your Own Adventure/cyoa.go
@@ -0,0 +1,14 @@
+package CYOA
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
